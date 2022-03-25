835feb099cae6190bb4fb8086adfb8139ff74edd
刺猬 <guoheng85@163.com>
2022-03-25 15:01:15 +0800

    注册功能已实现，下载功能以及数据查询的接口均未实现

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,26 +1,39 @@
 const LUSHAN_CONFIG = {
-	BASE_URL: 'http://106.12.109.129:8849/lushan',
+	BASE_URL: 'http://127.0.0.1:8081/lushan',
 	TIME_OUT: 100000
 }
 
-var instance = axios.create({
+const instance = axios.create({
 	baseURL: LUSHAN_CONFIG['BASE_URL'],
 	timeout: LUSHAN_CONFIG['TIME_OUT'],
 	headers: {
 		'Access-Control-Allow-Origin': '*',
 		'token': sessionStorage.getItem('')
 	}
-})
+});
+
+const fileUploadInstance = axios.create({
+	baseURL: LUSHAN_CONFIG['BASE_URL'],
+	timeout: LUSHAN_CONFIG['TIME_OUT'],
+	headers: {
+		'Access-Control-Allow-Origin': '*',
+		// 'token': sessionStorage.getItem(''),
+		'token': 'eFhyenM4STFZd0JrRGc1WlVacHhvNE0wNkJGdTN1SXRVNGJFNHRZeG4vRkRoVXk2S0FRbzFhTVMzYjA2ajJXNQ==',
+		'Conten-Type': "multipart/form-data"
+	}
+});
+
 
 /***
 	User Api
 */
+// 用户登录
 function userLogin(data) {
 	const {
 		email,
 		password
 	} = data
-	instance.post('/login', {
+	return instance.post('/login', {
 		'email': email,
 		'password': password
 	}).then(res => {
@@ -29,7 +42,13 @@ function userLogin(data) {
 		console.error(err)
 	})
 }
-
+// 用户注册
+function userRegister(userInfo) {
+	return instance.post(
+		'/register',
+		userInfo
+	)
+}
 
 /***
 	Device Api
@@ -42,14 +61,35 @@ function getDeviceInfo() {
 	DataFile Api
 */
 
+function downloadFile (url, formData) {
+    return fileUploadInstance.get(url, formData).then(res=>res)
+}
 
 
 /***
 	UserProof Api
 */
+function uploadUserProof(formData) {
+	return fileUploadInstance.post(
+		'/file/upload/user/proof',
+		formData
+	)
+}
 
 
 
 /***
 	Download Api
 */
+
+
+
+/***
+	Upload Api
+*/
+
+function uploadFile() {
+	return fileUploadInstance.post('/file/upload/user/proof', formData).then(res => {
+		res.data
+	})
+}
